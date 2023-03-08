8e04846e6c357e04cea52025cb61a91f148c5d52
Zozimo Fernandez <zozi.fer96@gmail.com>
2023-03-08 09:55:12 -0600

    Add env environment

--- pets/utils.py
+++ pets/utils.py
@@ -1,5 +1,8 @@
-from http import HTTPStatus
 import requests
+
+from http import HTTPStatus
+
+from django.conf import settings
 from rest_framework import exceptions
 
 
@@ -7,7 +10,7 @@ def get_dog_breeds() -> list[str] | None:
     """ This function retrieves a list of dog breeds from dog.ceo's Free API
     :return: A list of dog breeds
     """
-    url: str = 'https://dog.ceo/api/breeds/list/all'
+    url: str = settings.EXTERNAL_URLS['DOG_BREEDS_URL']
     r = requests.get(url)
     if r.status_code != HTTPStatus.OK:
         raise exceptions.NotFound(detail='Resource not found')
