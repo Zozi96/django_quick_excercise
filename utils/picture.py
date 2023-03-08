6d1b59c4cd1c22db72fa005f1aac380f4121c071
Zozimo Fernandez <zozi.fer96@gmail.com>
2023-03-08 11:36:26 -0600

    Remove unnecessary code

--- utils/picture.py
+++ utils/picture.py
@@ -8,8 +8,3 @@ def delete_profile_picture(sender, instance) -> None:
         if old_instance.profile_picture and instance.profile_picture != old_instance.profile_picture:
             # Delete the old picture only if the profile_picture field hasn't been updated
             default_storage.delete(old_instance.profile_picture.name)
-    if instance.profile_picture:
-        # Save the new picture on S3 Bucket
-        file_path = f"{settings.MEDIA_ROOT}/{instance.profile_picture.name}"
-        with default_storage.open(file_path, "wb") as f:
-            f.write(instance.profile_picture.file.read())
