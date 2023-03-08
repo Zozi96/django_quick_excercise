366fff15ab602e1932e47ae816d07709e8dde7a0
Zozimo Fernandez <zozi.fer96@gmail.com>
2023-03-08 10:21:49 -0600

    Change fields user

--- users/models.py
+++ users/models.py
@@ -9,11 +9,8 @@ class User(AbstractUser):
         regex=r"^\+?\d{9,15}$",
         message="Phone number must be entered in the format: '+999999999'. Up to 15 digits allowed.",
     )
-    phone_number = models.CharField(verbose_name="Phone Number", validators=(phone_regex,), max_length=17)
-    city = models.CharField(verbose_name="City", max_length=100)
+    phone_number = models.CharField(verbose_name="Phone Number", validators=(phone_regex,), max_length=17, blank=True)
+    city = models.CharField(verbose_name="City", max_length=100, blank=True)
     last_login = models.DateTimeField(verbose_name="Last login", auto_now=True)
     email = models.EmailField(verbose_name="Email address", unique=True)
     profile_picture = models.FileField(verbose_name='Profile picture', upload_to='users-profile-pictures', null=True)
-
-
-
